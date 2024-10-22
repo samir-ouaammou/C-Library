1ad61c6d64bda780311a793a09b5aacccfbc95e1
Samir Ouaammou <souaammo@e1r12p11.1337.ma>
2024-10-22 18:53:23 +0100

    1337 bg

---
+++ ft_strjoin.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souaammo <souaammo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 18:42:37 by souaammo          #+#    #+#             */
+/*   Updated: 2024/10/22 18:43:29 by souaammo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcat(char *dst, const char *src)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (dst[i])
+		i++;
+	j = 0;
+	while (src[j])
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst);
+}
+
+char	*ft_strcpy(char *dst, const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	size;
+	char	*res;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	size = ft_strlen(s1) + ft_strlen(s2);
+	res = (char *)malloc((size + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	ft_strcpy(res, s1);
+	ft_strcat(res, s2);
+	return (res);
+}
