dcabc370c5fb7822c9cb5b4bb735451700039f80
Samir Ouaammou <samirouaammou.4.4.2003@gmail.com>
2024-10-22 09:50:58 +0100

    1337 bg

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -5,8 +5,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	s_len;
 
+	if (!src)
+		return (0);
 	s_len = ft_strlen(src);
-	if (s_len <= size)
+	if (size == 0)
 		return (s_len);
 	i = 0;
 	while ((src[i]) && (i < size - 1))
