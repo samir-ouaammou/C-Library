d6b742bbfe9020e6f9eb1eb60cedd14fdb19d44d
Samir Ouaammou <souaammo@c4r4p8.1337.ma>
2024-10-25 22:42:04 +0100

    1337

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: souaammo <souaammo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 21:43:57 by souaammo          #+#    #+#             */
-/*   Updated: 2024/10/25 22:36:36 by souaammo         ###   ########.fr       */
+/*   Created: 2024/10/25 22:27:05 by souaammo          #+#    #+#             */
+/*   Updated: 2024/10/25 22:40:45 by souaammo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	size_t i;
-	char    *res;
+	size_t  i;
 
 	if ((!s) || (!f))
-		return (NULL);
-	res = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(char));
-	if (!res)
-		return (NULL);
+		return ;
 	i = 0;
 	while (s[i])
 	{
-		res[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	res[i] = '\0';
-	return (res);
 }
