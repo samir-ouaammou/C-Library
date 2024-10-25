244d5f835166e2f2105a3dd7329d25e47e10e831
Samir Ouaammou <souaammo@c4r4p8.1337.ma>
2024-10-25 21:37:08 +0100

    1337 bg

---
+++ ft_itoa.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souaammo <souaammo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 15:04:28 by souaammo          #+#    #+#             */
+/*   Updated: 2024/10/25 21:36:09 by souaammo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_countlen(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n < 0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t	len;
+	char	*res;
+
+	if (n == 0)
+	{
+		res = (char *)ft_calloc(2, sizeof(char));
+		if (!res)
+			return (NULL);
+		res[0] = '0';
+		res[1] = '\0';
+		return (res);
+	}
+	len = ft_countlen(n);
+	res = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	if (n < 0)
+	{
+		res[0] = '-';
+		n *= -1;
+	}
+	while (n != 0)
+	{
+		len--;
+		res[len] = (n % 10) + '0';
+		n = n / 10;
+	}
+	return (res);
+}
