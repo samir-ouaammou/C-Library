c31322cfb72b127c79410cd87d06ff84a32b8400
Samir Ouaammou <souaammo@c4r4p8.1337.ma>
2024-10-25 22:26:04 +0100

    1337 bg

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
-FILES_C = ft_isalpha.c ft_memcmp.c ft_strdup.c ft_strnstr.c \
-           ft_atoi.c ft_isascii.c ft_memcpy.c ft_strlcat.c ft_strrchr.c \
-           ft_bzero.c ft_isdigit.c ft_memmove.c ft_strlcpy.c ft_tolower.c \
-           ft_calloc.c ft_isprint.c ft_memset.c ft_strlen.c ft_toupper.c \
-           ft_isalnum.c ft_memchr.c ft_strchr.c ft_strncmp.c
+FILES_C = ft_atoi.c     ft_isdigit.c  ft_memmove.c  ft_strlcat.c  ft_strrchr.c \
+           ft_bzero.c    ft_isprint.c  ft_memset.c   ft_strlcpy.c  ft_strtrim.c \
+           ft_calloc.c   ft_itoa.c     ft_split.c    ft_strlen.c   ft_substr.c \
+           ft_isalnum.c  ft_memchr.c   ft_strchr.c   ft_strmapi.c  ft_tolower.c \
+           ft_isalpha.c  ft_memcmp.c   ft_strdup.c   ft_strncmp.c  ft_toupper.c \
+           ft_isascii.c  ft_memcpy.c   ft_strjoin.c  ft_strnstr.c
 
 OBJS = ${FILES_C:%.c=%.o}
 
 NAME = libft.a
 
 CC = cc
-C_FLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror
 
 RM = rm -rf
 AR = ar rc
@@ -19,8 +20,8 @@ all: $(NAME)
 $(NAME): $(OBJS)
 	$(AR) $(NAME) $(OBJS)
 
-$(OBJS): %.o: %.c
-	$(CC) $(C_FLAGS) -c $< -o $@
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
 	$(RM) $(OBJS)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: souaammo <souaammo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:54:13 by souaammo          #+#    #+#             */
-/*   Updated: 2024/10/25 14:58:34 by souaammo         ###   ########.fr       */
+/*   Updated: 2024/10/25 22:21:56 by souaammo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_isascii(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
 void	ft_bzero(void *s, size_t len);
 void	*ft_calloc(size_t count, size_t len);
 size_t	ft_strlen(const char *str);
@@ -34,7 +35,10 @@ char	*ft_strrchr(const char *str, int c);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_strncmp(const char *s1, const char *s2, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
 char	*ft_strnstr(const char *str, const char *needle, size_t len);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memchr(const void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t len);
